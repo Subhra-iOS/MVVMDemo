d1b49f3c116258686daf9313684325fcc84f0778
Subhra Roy <subhra.roy@e-arc.com>
2019-07-24 11:45:10 +0530

    ok

--- app/src/main/java/com/example/mvvmdemo/UserModel.kt
+++ app/src/main/java/com/example/mvvmdemo/UserModel.kt
@@ -1,11 +1,10 @@
 package com.example.mvvmdemo
 
-class Model {
+class UserModel {
 
     public var userId : String? = null
     public var id : String? = null
     public var title : String? = null
     public var body : String? = null
-
 
 }
